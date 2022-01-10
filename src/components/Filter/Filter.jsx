31b1624069de02c521f99db73e660c0f9cfbabef
Daria Khrushch <Daria140694@gmail.com>
2022-01-10 21:16:37 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import s from './Filter.module.css';
 import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 import contactsActions from '../../redux/contacts/contacts-actions';
 import { getFilter } from '../../redux/contacts/contacts-selectors';
+import s from './Filter.module.css';
 
 const Filter = () => {
   const value = useSelector(getFilter);
